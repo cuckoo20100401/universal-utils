4c0b4ba99878bfdee563cf1494e1b9312fa1fe1b
cuckoo20100401 <cuckoo20100401@163.com>
2019-12-03 16:06:59 +0800

    perfect project

--- src/main/java/org/cuckoo/universal/utils/ValidateUtils.java
+++ src/main/java/org/cuckoo/universal/utils/ValidateUtils.java
@@ -9,7 +9,16 @@ public class ValidateUtils {
 	 * @param value
 	 * @return
 	 */
-    public static boolean isMobile(String value) {
+    public static boolean isCellphone(String value) {
         return Pattern.matches("^((13[0-9])|(14[0-9])|(15[0-9])|(17[0-9])|(18[0-9]))\\d{8}$", value);
+    }
+    
+    /**
+     * 校验电话号
+     * @param value
+     * @return
+     */
+    public static boolean isTelephone(String value) {
+        return false;
     }
 }
